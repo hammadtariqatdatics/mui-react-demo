7b5aefdc5a3947c809f52a3e8f969f88f0fe8879
hammadtariqatdatics <126790893+hammadtariqatdatics@users.noreply.github.com>
2023-03-03 19:11:52 +0500

    Haliton-mui-demo

--- src/components/MuiThemeCustomize.jsx
+++ src/components/MuiThemeCustomize.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Typography } from "@mui/material";
 
 const MuiThemeCustomize = () => {
-  return <Typography>Hello worlds updated32</Typography>;
+  return <Typography>Hello worlds updated3</Typography>;
 };
 
 export default MuiThemeCustomize;
